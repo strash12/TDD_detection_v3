776cbca1f053af3be1a9707172ec14b796c1eff6
strash12 <58418676+strash12@users.noreply.github.com>
2023-07-13 13:36:53 +0700

    a

--- TDD_detection/RS_PROC.cpp
+++ TDD_detection/RS_PROC.cpp
@@ -88,7 +88,7 @@ namespace RS
       xvec SLOT_sig_fft = fft.fft(slotsig);
       xvec slot_sig_cut(SLOT_sig_fft.begin()+_param.fftsize/2-(2*_param.Nrb)*3,SLOT_sig_fft.begin()+_param.fftsize/2);
       slot_sig_cut.insert(slot_sig_cut.end(),SLOT_sig_fft.begin()+_param.fftsize/2+1,SLOT_sig_fft.begin()+_param.fftsize/2+(2*_param.Nrb)*3+1);
-      for(int i:slot_sig_cut)
+      for(xvec i:slot_sig_cut)
       {
         std::cout<<"ghghgh"<<std::endl;
         std::cout<<i<<std::endl;}
