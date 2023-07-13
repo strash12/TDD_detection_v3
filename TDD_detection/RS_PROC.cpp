d11308257d9ab0b10627646e84173dd08b846ccc
strash12 <58418676+strash12@users.noreply.github.com>
2023-07-13 14:05:04 +0700

    wq

--- TDD_detection/RS_PROC.cpp
+++ TDD_detection/RS_PROC.cpp
@@ -88,9 +88,10 @@ namespace RS
       xvec SLOT_sig_fft = fft.fft(slotsig);
       xvec slot_sig_cut(SLOT_sig_fft.begin()+_param.fftsize/2-(2*_param.Nrb)*3,SLOT_sig_fft.begin()+_param.fftsize/2);
       slot_sig_cut.insert(slot_sig_cut.end(),SLOT_sig_fft.begin()+_param.fftsize/2+1,SLOT_sig_fft.begin()+_param.fftsize/2+(2*_param.Nrb)*3+1);
+      std::cout<<"ghghgh"<<std::endl;
       for(int i = 0; i <slot_sig_cut.size();i++)
       {
-        std::cout<<"ghghgh"<<std::endl;
+        
         std::cout<<slot_sig_cut[i]<<std::endl;
         }
       gen_array(slot_sig_cut);
@@ -104,7 +105,7 @@ namespace RS
         
         for (int i = 0; i < _param.Nrb*2;i++){
             int a = rs_space_index[i][d]-1;
-            std::cout<<a<<std::endl;
+           // std::cout<<a<<std::endl;
                 framesig_RS[k] = Slot[a];
                 ref_RS[k] = xd(rs_signal_fixed_real[i][j],rs_signal_fixed_imag[i][j]);
                 k++;
